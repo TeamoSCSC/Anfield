7a28ef45f6365e079937e72d950dbddff6ce2c99
shenchen <654958337@qq.com>
2019-07-11 13:52:46 +0800

    'resetpassword_token'

--- routes/resetpassword.py
+++ routes/resetpassword.py
@@ -1,5 +1,5 @@
 import uuid
-
+import json
 from flask import (
     render_template,
     request,
@@ -14,25 +14,17 @@ from models.user import User
 from routes import current_user
 
 from models.topic import Topic
-from routes.helper import new_csrf_token
+from routes.helper import new_csrf_token, cache
 
 main = Blueprint('reset', __name__)
-
-
-def reset_token():
-    token = str(uuid.uuid4())
-    return token
-
-
-user_token = {}
 
 
 @main.route("/send", methods=['POST'])
 def reset_send_mail():
     username = request.form['username']
     u = User.one(username=username)
-    token = reset_token()
-    user_token[token] = u.id
+    token = new_csrf_token()
+    u.id = json.loads(cache.get(token))
     Messages.send(
         title='sc论坛重置密码',
         content='http://152.136.129.239/reset/view?token={}'.format(token),
@@ -45,21 +37,21 @@ def reset_send_mail():
 @main.route("/view", methods=['GET'])
 def reset_view():
     print('???')
-    token = str(request.args.get('token', None))
-    u_id = user_token.get(token, -1)
-    if token is not None and u_id != -1:
+    token = request.args.get('token', None)
+    u_id = json.loads(cache.get(token))
+    if token is not None and u_id is not None:
         u = User.one(id=u_id)
         return render_template('reset.html', user=u, token=token)
     else:
         return redirect(url_for('index.index'))
 
 
-@main.route("/update/token=<string:token>", methods=['POST'])
+@main.route("/update/token=<token>", methods=['POST'])
 def reset_update(token):
     print('321')
-    u_id = user_token.get(token, -1)
+    u_id = json.loads(cache.get(token))
     password = request.form['password']
-    if u_id != -1 and len(password) > 2:
+    if u_id is not None and len(password) > 2:
         u = User.one(id=u_id)
         u.password = u.salted_password(password)
         User.update(u_id, password=u.password)
