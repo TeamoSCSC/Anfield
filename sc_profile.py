7648a7d10380e78d2123866c41af33a9822d4dfd
shenchen <654958337@qq.com>
2019-07-30 13:07:44 +0800

    'cprofile_function'

---
+++ sc_profile.py
@@ -0,0 +1,27 @@
+import app
+from routes.personal import index
+import cProfile
+from pstats import Stats
+
+
+def profile_request(path, cookie, f):
+    a = app.configured_app()
+    pr = cProfile.Profile()
+    headers = {'Cookie': cookie}
+
+    with a.test_request_context(path, headers=headers):
+        pr.enable()
+        f(7)
+        pr.disable()
+
+    pr.create_stats()
+    s = Stats(pr).sort_stats('cumulative')
+    s.dump_stats('profile.pstat')
+
+    s.print_stats('.*Anfield.*')
+
+
+if __name__ == '__main__':
+    path = '/personal/7'
+    cookie = 'session=user78f2482d-92c5-4923-8b79-0e890d432b78'
+    profile_request(path, cookie, index)
